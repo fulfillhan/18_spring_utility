42bb655cc198392e45a1a1f01eb71ffcaf47cc07
sanghui <tkdgml719@gamil.com>
2024-03-04 15:00:40 +0900

    커밋

--- 18_spring_utility/src/main/java/com/application/utility/file/FileController.java
+++ 18_spring_utility/src/main/java/com/application/utility/file/FileController.java
@@ -1,6 +1,5 @@
 package com.application.utility.file;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -66,98 +65,91 @@ import java.nio.file.Paths;
 public class FileController {
 
 	/*
-	  
-	   웹애플리케이션의 설정정보 위치를 통합하기 위하여 
-	   application.properties파일에서 파일저장 위치를 명시하고 @Value 애노테이션을 사용하여 
-	   file.repo.path경로 대입
-	   @Value 를 사용하는 이유는? import org.springframework.beans.factory.annotation.Value;
-	 
+	 * 
+	 * 웹애플리케이션의 설정정보 위치를 통합하기 위하여 application.properties파일에서 파일저장 위치를 명시하고 @Value
+	 * 애노테이션을 사용하여 file.repo.path경로 대입
+	 * 
+	 * @Value 를 사용하는 이유는? import org.springframework.beans.factory.annotation.Value;
+	 * 
 	 */
 	@Value("${file.repo.path}")
 	private String fileRepositoryPath;
-	
-	
+
 	@GetMapping("/fileEx")
 	public String fileEx01() {
 		return "file/fileEx";
 	}
-	
-	
-	
+
 	// 1) 파일 업로드 기초 예시
-	
 	@PostMapping("/upload1")
-	                   // @RequestParam("name명") 어노테이션을 사용하여 MultipartFile타입으로 파일을 전달받는다. 
-	public String upload1(@RequestParam("upFile") MultipartFile upfile) throws IllegalStateException, IOException {
-		
-//		System.out.println("file name : "+upfile.getOriginalFilename());
-//		System.out.println("file contentType : "+ upfile.getContentType());
-		
-		File targetFile =new File(fileRepositoryPath + upfile.getOriginalFilename());// 파일객체를 생성한다.
-		upfile.transferTo(targetFile); // transferTo(파일객체) 메서드를 사용하여 파일저장소에 파일을 저장한다.
-									   // 해당 작업을 해야 파일에저장된다.
-		
-		//upfile.transferTo(new File(fileRepositoryPath + upfile.getOriginalFilename())); 이렇게 작성도 가능하다.
+	// @RequestParam("name명") 어노테이션을 사용하여 MultipartFile타입으로 파일을 전달받는다.
+	public String upload1(@RequestParam("upFile") MultipartFile upFile) throws IllegalStateException, IOException {
+
+//			System.out.println(multipartFile.getOriginalFilename());
+//			System.out.println(multipartFile.getContentType());
+
+		File targetFile = new File(fileRepositoryPath + upFile);// 파일객체를 생성한다.
+		// upfile.transferTo(new File(fileRepositoryPath +
+		// upfile.getOriginalFilename())); 이렇게 작성도 가능하다.
+		upFile.transferTo(targetFile);//// transferTo(파일객체) 메서드를 사용하여 파일저장소에 파일을 저장한다.
+		// 해당 작업을 해야 파일에저장된다.
+
 		return "redirect:/file/fileEx";
 	}
+
 	// 2) 다중파일 업로드 + 파일명 수정(UUID 적용) 예시
-	
 	@PostMapping("/upload2")
-	                   // @RequestParam("name명") 어노테이션을 사용하여 List<MultipartFile>타입으로 파일을 전달받는다.
+	// @RequestParam("name명") 어노테이션을 사용하여 List<MultipartFile>타입으로 파일을 전달받는다.
 	public String upload2(@RequestParam("files") List<MultipartFile> files) throws IllegalStateException, IOException {
-		
+
 		for (MultipartFile file : files) {
-			
-			if(!file.isEmpty()) {// 파일있어야지 올리니깐 empty()가 아니여야함
-				//원본파일이름
-				 String originalFilename = file.getOriginalFilename();
-				
-				 //범용고용식별자(UUID) 생성
-				 UUID uuid = UUID.randomUUID();
-				 
-				 //확장자 추출
-				 String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-				
-				//업로드 파일 이름 수정
-				String uploadFileName= uuid + extension;
-				
-				//transferTo() 메서드를 사용하여 파일 업로드 구현
-				//file.transferTo(new File(fileRepositoryPath+ file.getOriginalFilename()));
-				file.transferTo(new File(fileRepositoryPath+ uploadFileName));
-				
+			if (!file.isEmpty()) {//파일이 비어있지 않다면(파일이 비어있지않아야지 다음 구문실행)
+				// 원본파일이름가지고오기
+				String originalFilename = file.getOriginalFilename();
+				// 범용고용식별자(UUID) 생성
+				UUID uuid = UUID.randomUUID();
+
+				// 확장자 추출
+				String extenstion = originalFilename.substring(originalFilename.indexOf("."));
+
+				// 업로드 파일 이름 수정
+				String uploadFileName = uuid + extenstion;
+
+				// transferTo() 메서드를 사용하여 파일 업로드 구현
+				// file.transferTo(new File(fileRepositoryPath+ file.getOriginalFilename()));
+				file.transferTo(new File(fileRepositoryPath + uploadFileName));
 			}
 		}
 		return "redirect:/file/fileEx";
 	}
-	
+
 	/*
-	
-		import org.springframework.core.io.InputStreamResource;
-		import org.springframework.core.io.Resource;
-		import org.springframework.core.io.UrlResource;
-		import org.springframework.http.ContentDisposition;
-		import org.springframework.http.HttpHeaders;
-		import org.springframework.http.HttpStatus;
-		import org.springframework.http.ResponseEntity;
-		import java.nio.file.Path;
-		import java.nio.file.Paths;
-		
-	*/
-	
-	
+	 * 
+	 * import org.springframework.core.io.InputStreamResource; import
+	 * org.springframework.core.io.Resource; import
+	 * org.springframework.core.io.UrlResource; import
+	 * org.springframework.http.ContentDisposition; import
+	 * org.springframework.http.HttpHeaders; import
+	 * org.springframework.http.HttpStatus; import
+	 * org.springframework.http.ResponseEntity; import java.nio.file.Path; import
+	 * java.nio.file.Paths;
+	 * 
+	 */
+
 	// 3) 썸네일기능 구현 예시
 	@GetMapping("/thumbnails")
 	@ResponseBody
 	public Resource thumbnails(@RequestParam("fileName") String fileName) throws MalformedURLException {
+		//new UrlResource("file :"+ fileRepositoryPath+fileName);
 		// new UrlResource("file:" + 파일접근경로) 객체를 반환하여 이미지를 조회한다.
-		return  new UrlResource("file:" + fileRepositoryPath+fileName); // 이미지 경로 수정하여 사용
+		return new UrlResource("file :"+ fileRepositoryPath+fileName);// 이미지 경로 수정하여 사용
 	}
 	
-	// 4) 파일 다운로드 기능 구현 예시
-	@GetMapping("/downloadFile")
-	public ResponseEntity<Object> downloadFile(@RequestParam("fileName")String fileName) throws MalformedURLException{
-		
-		String downloadFile = fileRepositoryPath + fileName ;  // 이미지 경로를 수정하여 사용
+
+	// 4) 파일 다운로드 기능 구현 예시	
+	@GetMapping("downloadFile")
+	public ResponseEntity<Object> downloadFile(@RequestParam("fileName") String fileName) {
+		String downloadFile = fileRepositoryPath+fileName;//파일 경로
 		
 		try {
 			
@@ -171,56 +163,45 @@ public class FileController {
 			return new ResponseEntity<Object>(resource, headers, HttpStatus.OK);
 			
 		} catch(Exception e) {
+			
 			return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
 		}
-	        
+		
 	}
 
-	
-	
 	// 5) 파일 삭제 예시
-	
 	@PostMapping("/delete")
 	public String delete(@RequestParam("deleteFileName") String deleteFileName) {
-		
-		//new File(fileRepositoryPath+deleteFileName).delete(); //파일 삭제
+		 File file = new File(fileRepositoryPath+deleteFileName);
+		 
+		 if(file.exists()) {// 파일이 존재하면
+			 file.delete(); // 삭제한다
+		 }
+		 return "redirect:/file/fileEx";
+	}
+
+	// 6) 파일 수정 예시(지우고 업데이트 한다는 전제하)
+	@PostMapping("/update")
+	public String update(@RequestParam("deleteFileName") String deleteFileName,
+			              @RequestParam("modifyFile") MultipartFile modifyFile) throws IllegalStateException, IOException {
 		
 		File deleteFile = new File(fileRepositoryPath+deleteFileName);
 		
-		if(deleteFile.exists()) { //파일이 존재하면
-			deleteFile.delete(); // 파일을 삭제한다.
+		if(deleteFile.exists() && !modifyFile.isEmpty()) {
+			deleteFile.delete();// 파일 삭제한다.
 			
+			 String  originalFilename = modifyFile.getOriginalFilename();
+			 	
+			 UUID uuid = UUID.randomUUID();
+			 
+			 String extension = originalFilename.substring(originalFilename.indexOf("."));
+			 
+			 String uploadFile = uuid + extension;
+			 
+			 modifyFile.transferTo(new File(fileRepositoryPath+uploadFile));
 		}
-		return "redirect:/file/fileEx";
-	}
-	
-	
-	// 6) 파일 수정 예시(지우고 업데이트 한다는 전제하)
-	@PostMapping("/update")
-	public String update(@RequestParam("deleteFileName") String deleteFileName , @RequestParam("modifyFile")MultipartFile modifyFile) throws IllegalStateException, IOException{
 		
-		 File deleteFile = new File(fileRepositoryPath+deleteFileName);
-		 if(deleteFile.exists() && !modifyFile.isEmpty()) {
-
-				deleteFile.delete(); // 파일 삭제
-				
-				// 원본 파일 이름
-				String originalFilename = modifyFile.getOriginalFilename();
-				
-				// 범용고유식별자(UUID) 생성
-				UUID uuid = UUID.randomUUID();
-				
-				// 원본 파일 확장자 추출
-				String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-				
-				// 업로드 파일 이름 수정
-				String uploadFileName = uuid + extension;
-				
-				// transferTo() 메서드를 사용하여 파일 업로드 구현
-				modifyFile.transferTo(new File(fileRepositoryPath + uploadFileName));
-		 }
-		 
 		return "redirect:/file/fileEx";
+		
 	}
-	
 }
